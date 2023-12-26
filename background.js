ac66ae10bd4ea271fa8f5052350d9a382a77079a
winstonl <wrlucasas@gmail.com>
2023-12-25 16:35:30 -0800

    fixed the code up a bit more and added a periodic check for tab age

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
 let tabTimes = {};
+let tabDetails = {};
 let bookmarkFolderId = null;
 
 
 // Record opening time of new tab
 chrome.tabs.onCreated.addListener((tab) => {
     tabTimes[tab.id] = Date.now();
+    tabDetails[tab.id] = { title: tab.title, url: tab.url};
     console.log("Tab created: ", tab.id, " at ", tabTimes[tab.id]);
 });
 //  Logs updates for when a tab is updated (i.e. Changing/refreshing page)
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    tabDetails[tabId] = { title : tab.title, url: tab.url };
     if (changeInfo.status === 'complete' && tab.active) {
         console.log("Tab updated: ", tabId, changeInfo);
     }
@@ -17,32 +20,56 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 // Retrieves the structure of the bookmark tree
 // Identifies the bookmarks bar
 // Creates new bookmark folder
-chrome.bookmarks.getTree((results) => {
-    let bookmarkBar = results[0].children[0];
-    let folderName = 'Closed Tabs';
+async function checkOrCreateFolder(folderName) {
+    const results = await new Promise((resolve) => chrome.bookmarks.getTree(resolve));
+    const bookmarkBar = results[0].children[0];
+    
+    const existingFolders = await new Promise((resolve) => chrome.bookmarks.search({ title: folderName }, resolve));
+    const existingFolder = existingFolders.find(b => b.parentId === bookmarkBar.id && b.title === folderName);
 
-    // Search if the folder already exists
-    chrome.bookmarks.search({ title: folderName }, (results) => {
-        let existingFolder = results.find(b => b.parentId === bookmarkBar.id && b.title === folderName);
+    if (!existingFolder) {
+        const newFolder = await new Promise((resolve) => chrome.bookmarks.create({
+            'parentId': bookmarkBar.id,
+            'title': folderName
+        }, resolve));
+        console.log("Added folder: " + newFolder.title);
+        return newFolder.id;
+    } else {
+        console.log("Folder already exists");
+        return existingFolder.id;
+    }
+}
 
-        if (!existingFolder) {
-            // Folder doesn't exist, create it
-            chrome.bookmarks.create({
-                'parentId': bookmarkBar.id,
-                'title': folderName
-            }, (newFolder) => {
-                console.log("Added folder: " + newFolder.title);
-            });
-        } else {
-            // Folder exists, you can skip creation
-            console.log("Folder already exists");
+
+
+// Function to handle tab processing
+async function processTab(tabId) {
+    try {
+        let openDuration = Date.now() - tabTimes[tabId];
+        console.log("Tab was open for: ", openDuration, " hours.");
+
+        if (openDuration > 48 * 60 * 60 * 1000) {
+            const folderId = await checkOrCreateFolder("Clutter");
+            const tabInfo = tabDetails[tabId];
+            if (tabInfo && tabInfo.url) {
+                chrome.bookmarks.create({ parentId: folderId, title: tabInfo.title, url: tabInfo.url });
+            }
+            chrome.tabs.remove(tabId);
         }
+    } catch (error) {
+        console.error("Error processing tab: ", error);
+    }
+}
+
+// Setup periodic check for tabs
+setInterval(() => {
+    chrome.tabs.query({}, (tabs) => {
+        tabs.forEach(tab => processTab(tab.id));
     });
-});
+}, 1000 * 60 * 60); // Check every hour
 
-
-
+// Clean up on tab removal
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    let openDuration = Date.now() - tabTimes[tabId];
-    console.log("Tab was open for: ", openDuration, " hours.")
+    delete tabTimes[tabId];
+    delete tabDetails[tabId];
 });
